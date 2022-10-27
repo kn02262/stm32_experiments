ed4f3839e90f0d5a0d4b3569c7ebf5a59aecd5b9
Nikita Kolesnikov <nikolesnikov100@gmail.com>
2022-10-27 17:43:28 +0200

    pract3 LED blinking with button trigger switch

--- main.c
+++ main.c
@@ -1,25 +1,30 @@
 #include <stdint.h>
-#include <stm32f10x.h>
+#include "stm32f10x.h"
 
-void delay(uint32_t ticks) {
-	for (int i=0; i<ticks; i++) {
-		__NOP();
-	}
-}
-
-int __attribute((noreturn)) main(void) {
-	// Enable clock for AFIO
-	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-	// Enable clock for GPIOC
+int main(void)
+{
 	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	// Enable PC13 push-pull mode
-	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+	GPIOC->CRH = GPIOC->CRH & ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13) | GPIO_CRH_MODE13_0; //PC13 = output
+	GPIOC->CRH = GPIOC->CRH & ~(GPIO_CRH_CNF14 | GPIO_CRH_MODE14) | GPIO_CRH_CNF14_1; //PC14 = Input
+	GPIOC->ODR |= GPIO_ODR_ODR14; // Enable PC14 Pull-up
+	uint32_t i;
+	uint8_t button_state = 0xFF;
+    
+    while(1)
+    {
+		if(!(GPIOC->IDR & GPIO_IDR_IDR14)){ // Button is pressed
+			button_state = ~button_state;
+			for(i=0; i<300000; i++){ __NOP(); }; // Debouncing
+			while(!(GPIOC->IDR & GPIO_IDR_IDR14)); // Wait the button to be released
+		}
 
-    while (1) {
-	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
-		delay(100000);
-	    GPIOC->ODR &= ~(1U<<13U);
-	    delay(100000);
+		if(button_state){
+			GPIOC->ODR &= ~GPIO_ODR_ODR13;
+			for(i=0; i<300000; i++){ __NOP(); };
+
+			GPIOC->ODR |= GPIO_ODR_ODR13;
+			for(i=0; i<300000; i++){ __NOP(); };
+		}
+
     }
 }
